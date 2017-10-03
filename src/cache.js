e2fb935ed9d185c48d1ede9b4fc904ea40d3c850
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2017-10-03 12:24:29 +0200

    Make Redis optional

--- src/cache.js
+++ src/cache.js
@@ -5,29 +5,33 @@ bluebird.promisifyAll(redis.RedisClient.prototype)
 export default function (config) {
     let module = {}
 
-    let redisOptions = { auth_pass: config.redis.pass }
-    if (config.redis.ssl) {
-        redisOptions.tls = { servername: config.redis.host }
+    let redisClient
+
+    if (config.redis) {
+        let redisOptions = { auth_pass: config.redis.pass }
+        if (config.redis.ssl) {
+            redisOptions.tls = { servername: config.redis.host }
+        }
+    
+        redisClient = redis.createClient(config.redis.port, config.redis.host, redisOptions)
+        process.on('exit', () => redisClient.quit())
     }
 
-    const redisClient = redis.createClient(config.redis.port, config.redis.host, redisOptions)
-    process.on('exit', () => redisClient.quit())
-
     module.exists = async key => {
-        return config.redis ? await redisClient.existsAsync(key) : false
+        return redisClient ? await redisClient.existsAsync(key) : false
     }
 
     module.get = async key => {
-        return config.redis ? await redisClient.getAsync(key) : undefined
+        return redisClient ? await redisClient.getAsync(key) : undefined
     }
 
     module.set = (key, value) => {
-        if (config.redis)
+        if (redisClient)
             redisClient.set(key, value)
     }
 
     module.flush = async () => {
-        if (config.redis)
+        if (redisClient)
             await redisClient.flushdbAsync()
     }
 
