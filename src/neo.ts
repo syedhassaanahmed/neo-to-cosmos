4ed99feb43e295508ec2580dcb70dd2ed16434a4
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2018-03-10 21:13:18 +0100

    Remove unnecessary bolt logging

--- src/neo.ts
+++ src/neo.ts
@@ -8,8 +8,6 @@ export default class Neo {
     constructor(pageSize: Number, logger: LoggerInstance) {
         this.logger = logger;
         this.pageSize = pageSize;
-
-        this.logger.info(`Neo4j Bolt: ${process.env.NEO4J_BOLT}`);
     }
 
     getTotalNodes = async () => {
