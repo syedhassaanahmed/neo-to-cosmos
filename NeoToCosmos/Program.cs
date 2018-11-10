824a8b184adda59a32ebfacf8b28f9aa844c125a
Syed Hassaan Ahmed <syah@microsoft.com>
2018-11-10 14:13:54 +0100

    Bootstrap .net core project

---
+++ NeoToCosmos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeoToCosmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
