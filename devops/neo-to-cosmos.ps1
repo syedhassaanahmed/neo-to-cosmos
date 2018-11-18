99dfc3dd8c23b699873c4dd67d593b6ce2c0d08b
Syed Hassaan Ahmed <syah@microsoft.com>
2018-11-19 00:09:44 +0100

    Add script for functional tests

--- devops/neo-to-cosmos.ps1
+++ devops/neo-to-cosmos.ps1
@@ -7,25 +7,35 @@ $env:COSMOSDB_OFFERTHROUGHPUT = "10000"
 $env:NEO4J_USERNAME = "neo4j"
 $env:NEO4J_PASSWORD = "Neo4j"
 
+$CONTAINER_NAME="neo4j-got"
 $NEO4J_BOLT_PORT=7687
 $env:NEO4J_BOLT = "bolt://localhost:$NEO4J_BOLT_PORT"
 $NEO4J_HTTP_PORT=7474
 
-docker run --name "neo4j-got" -dit -p ${NEO4J_BOLT_PORT}:${NEO4J_BOLT_PORT} `
+try { docker rm -f $CONTAINER_NAME } catch {}
+
+docker run --name $CONTAINER_NAME -d -p ${NEO4J_BOLT_PORT}:${NEO4J_BOLT_PORT} `
     -p ${NEO4J_HTTP_PORT}:${NEO4J_HTTP_PORT} `
     -e NEO4J_AUTH=$env:NEO4J_USERNAME/$env:NEO4J_PASSWORD `
     syedhassaanahmed/neo4j-game-of-thrones
 
 & 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' /noui
 
+function Test-Http ($endpoint) {
+    $response = try { (Invoke-WebRequest -Uri $endpoint -ErrorAction Stop).BaseResponse } 
+        catch [System.Net.WebException] {}
+    
+    return [int]$response.BaseResponse.StatusCode
+}
+
 do {
     Write-Host "waiting for Neo4j server to start..."
     Start-Sleep 3
-} until(Test-NetConnection localhost -Port $NEO4J_HTTP_PORT | Where-Object { $_.TcpTestSucceeded } )
+} until((Test-Http "http://localhost:$NEO4J_HTTP_PORT") -ne 200)
 
 do {
     Write-Host "waiting for Cosmos DB emulator to start..."
     Start-Sleep 3
-} until(Test-NetConnection localhost -Port $COSMOSDB_PORT | Where-Object { $_.TcpTestSucceeded } )
+} until((Test-Http "https://localhost:$COSMOSDB_PORT") -ne 401)
 
-dotnet run --project .\NeoToCosmos\NeoToCosmos.csproj -- -r
+dotnet run --project .\NeoToCosmos\NeoToCosmos.csproj
