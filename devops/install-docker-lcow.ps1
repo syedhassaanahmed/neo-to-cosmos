7bb320d196b46026c07930c317976ea269a2d748
Syed Hassaan Ahmed <syah@microsoft.com>
2018-11-19 20:59:44 +0100

    Fix powershell TLS error

--- devops/install-docker-lcow.ps1
+++ devops/install-docker-lcow.ps1
@@ -1,13 +1,11 @@
 Stop-Service *docker*
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Invoke-WebRequest -UseBasicParsing -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
 Invoke-WebRequest -UseBasicParsing -OutFile docker.exe https://master.dockerproject.org/windows/x86_64/docker.exe
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -UseBasicParsing -OutFile release.zip https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip
 
-Remove-Item "$env:ProgramFiles\Linux Containers" -Force -Recurse
-Expand-Archive release.zip -DestinationPath "$Env:ProgramFiles\Linux Containers\."
-Remove-Item release.zip
+Expand-Archive release.zip -DestinationPath "$Env:ProgramFiles\Linux Containers\." -Force
 
 .\dockerd.exe -D --experimental
