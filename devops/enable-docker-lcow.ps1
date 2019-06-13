6188d2157aa0b8a948d01fe279a4460b9c7e6958
Syed Hassaan Ahmed <syah@microsoft.com>
2019-06-13 23:25:41 +0200

    Install Windows Container features

--- devops/enable-docker-lcow.ps1
+++ devops/enable-docker-lcow.ps1
@@ -10,26 +10,30 @@ if ((Get-WindowsOptionalFeature -FeatureName Containers -Online).State -ne 'Enab
 }
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -OutFile docker.zip https://master.dockerproject.org/windows/x86_64/docker.zip
+#Invoke-WebRequest -OutFile docker.zip https://master.dockerproject.org/windows/x86_64/docker.zip
+Invoke-WebRequest -OutFile docker.exe https://master.dockerproject.org/windows/x86_64/docker.exe
+Invoke-WebRequest -OutFile dockerd.exe https://master.dockerproject.org/windows/x86_64/dockerd.exe
 Invoke-WebRequest -OutFile release.zip https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip
 
-Expand-Archive docker.zip -DestinationPath "$Env:ProgramFiles\Docker\." -Force
+#Expand-Archive docker.zip -DestinationPath "$Env:ProgramFiles\Docker\." -Force
 Expand-Archive release.zip -DestinationPath "$Env:ProgramFiles\Linux Containers\." -Force
 
-$DOCKER_DAEMON_CONFIG="$env:programdata\docker\config\daemon.json"
-if (!(Test-Path $DOCKER_DAEMON_CONFIG)) {
-    New-Item -ItemType "file" -Path $DOCKER_DAEMON_CONFIG -Value "{}" -Force
-}
-$DOCKER_DAEMON_JSON = Get-Content $DOCKER_DAEMON_CONFIG | Out-String | ConvertFrom-Json
-$DOCKER_DAEMON_JSON | Add-Member -Type NoteProperty -Name 'experimental' -Value $True -Force
-$DOCKER_DAEMON_JSON | ConvertTo-Json | Set-Content $DOCKER_DAEMON_CONFIG
+# $DOCKER_DAEMON_CONFIG="$env:programdata\docker\config\daemon.json"
+# if (!(Test-Path $DOCKER_DAEMON_CONFIG)) {
+#     New-Item -ItemType "file" -Path $DOCKER_DAEMON_CONFIG -Value "{}" -Force
+# }
+# $DOCKER_DAEMON_JSON = Get-Content $DOCKER_DAEMON_CONFIG | Out-String | ConvertFrom-Json
+# $DOCKER_DAEMON_JSON | Add-Member -Type NoteProperty -Name 'experimental' -Value $True -Force
+# $DOCKER_DAEMON_JSON | ConvertTo-Json | Set-Content $DOCKER_DAEMON_CONFIG
 
-Start-Service $DOCKER_SERVICES
+# Start-Service $DOCKER_SERVICES
 
-do
-{
-    Write-Host "waiting for Docker to start..."
-    Start-Sleep 1
-} until ((Get-Service $DOCKER_SERVICES | Where-Object {$_.status -eq "Running"}).count -ge 1)
+# do
+# {
+#     Write-Host "waiting for Docker to start..."
+#     Start-Sleep 1
+# } until ((Get-Service $DOCKER_SERVICES | Where-Object {$_.status -eq "Running"}).count -ge 1)
+
+.\dockerd.exe -D --experimental
 
 docker info
